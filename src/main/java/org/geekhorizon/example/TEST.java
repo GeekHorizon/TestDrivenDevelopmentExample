f2262ec7b7c477c59b5f33e65e7d82f845e940ed
hyeokcheol <hyeokcheol@192.168.2.2>
2016-11-18 14:20:30 +0900

    Test

---
+++ src/main/java/org/geekhorizon/example/TEST.java
@@ -0,0 +1,5 @@
+package org.geekhorizon.example;
+
+public class TEST {
+
+}
---
+++ src/test/java/org/geekhorizon/example/money/MoneyTest.java
@@ -0,0 +1,14 @@
+package org.geekhorizon.example.money;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
